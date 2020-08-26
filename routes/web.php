c96af0c404bc39b8fa3f5e8086ff3156e13c7e62
Md. Hanif <mdhanif6731@gmail.com>
2020-08-26 20:16:34 +0600

    complete labtask => edit + delete + details

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
+Route::get('/login','loginController@index');
+
+Route::post('/login',['uses'=>'loginController@validation']);
+
+Route::get('/home','HomeController@index');
+Route::get('/home/edit/{id}', 'HomeController@edit');
+Route::post('/home/edit/{id}', 'HomeController@update');
+Route::get('/home/delete/{id}', 'HomeController@delete');
+Route::post('/home/delete/{id}', 'HomeController@destroy');
+Route::get('/home/details/{id}','HomeController@details');
